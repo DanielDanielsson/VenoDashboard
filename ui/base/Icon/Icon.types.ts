1cf8523d4e6b2b6ab8df6adaaff5545b5f108836
DanielDanielsson <danielssondaniel94@gmail.com>
2026-03-25 09:21:32 +0100

    refactor(icons): remove unused sprite symbols

--- ui/base/Icon/Icon.types.ts
+++ ui/base/Icon/Icon.types.ts
@@ -1,18 +1,12 @@
 import type { Stylable } from '../../types';
 
 export type IconName =
-  | 'chevron-right'
-  | 'chevron-down'
-  | 'search'
-  | 'menu'
-  | 'menu-close'
   | 'sun'
   | 'moon'
   | 'home'
   | 'activity'
   | 'info'
   | 'settings'
-  | 'plug'
   | 'key'
   | 'glucose'
   | 'veno-logo';
